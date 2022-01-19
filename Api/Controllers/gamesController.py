a094e3ec75d36caa6e4d0c2b6a7ff9b59403427b
derciosinione <derciosinione@gmail.com>
2022-01-19 04:08:16 +0100

    Created Games Controllers

--- Api/Controllers/gamesController.py
+++ Api/Controllers/gamesController.py
@@ -1,5 +1,12 @@
-from fastapi import APIRouter, status, Response
+import os, shutil
+# import shutil
+from uuid import uuid4
+from typing import Any
+from fastapi import APIRouter, status, Response, File, UploadFile
 from bson import ObjectId
+from PIL import Image
+import PIL
+from starlette.routing import request_response 
 
 from Api.Config.db import db
 from Api.Models.Dto.CreateGameDto import CreateGame
@@ -49,3 +56,31 @@ async def deleteGame(id, response: Response):
     
     db.games.find_one_and_delete({"_id": ObjectId(id)})
     return None
+
+@gamesRoutes.post('/games/image-upload', status_code=status.HTTP_200_OK)
+async def uploadGameImage(file: UploadFile = File(...)):
+    save_picture(file)
+    # file.filenam
+    # with open('test1.jpg', 'wb') as buffer:
+    #     shutil.copyfileobj(file.file, buffer)
+    #     buffer.close()
+    return {"filename": file.filename, "contentType": file.content_type}
+
+
+@gamesRoutes.post('/games/create-file', status_code=status.HTTP_200_OK)
+async def uploadGameImage(file: bytes = File(...)):
+    return { "size": len(file)}
+
+
+def save_picture(picture):
+    randon_uid = str(uuid4())
+    _, f_ext = os.path.splitext(picture.filename)
+    picture_fn = randon_uid + f_ext
+    picture_path = os.path.join(picture_fn)
+
+    output_size = (125,125)
+    img = Image.open(picture.file)
+    img.thumbnail(output_size)
+    img.save(picture_path)
+
+    return picture_fn
