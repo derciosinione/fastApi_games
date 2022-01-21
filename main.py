f77e54fd8ba755edcd299186b0a9ad16dcc7406a
derciosinione <derciosinione@gmail.com>
2022-01-21 03:49:18 +0100

    Added Static files routes

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
+from fastapi.staticfiles import StaticFiles
 
 from Api.Controllers.gamesRoutes import gamesRoutes
 from Api.Controllers.defaultRoute import defaultRoute
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 app.include_router(gamesRoutes)
 app.include_router(defaultRoute)
 
